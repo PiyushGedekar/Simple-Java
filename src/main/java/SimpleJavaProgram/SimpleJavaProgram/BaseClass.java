94227b2c181910b5334c20f129854168fc1fff38
PiyushGedekar <pgedekar@gmail.com>
2020-06-24 16:13:59 +0530

    New change-1

---
+++ src/main/java/SimpleJavaProgram/SimpleJavaProgram/BaseClass.java
@@ -0,0 +1,11 @@
+package main.java.SimpleJavaProgram.SimpleJavaProgram;
+
+public class BaseClass {
+	
+	public static void increament(int cnt){
+		
+		cnt++;
+	System.out.println("Increamented value=="+ cnt);
+	}
+
+}
---
+++ src/test/java/SimpleJavaProgram/SimpleJavaProgram/TestClass.java
@@ -0,0 +1,14 @@
+package test.java.SimpleJavaProgram.SimpleJavaProgram;
+
+import main.java.SimpleJavaProgram.SimpleJavaProgram.BaseClass;
+
+public class TestClass extends BaseClass {
+
+	public static void main(String[] args){
+		
+		int n=10;
+		
+		TestClass.increament(n);
+	}
+	
+}
